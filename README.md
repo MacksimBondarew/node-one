d1a121df10fdedc5f3570f3768e4a3626f8bfb97
MacksimBondarew <112969071+MacksimBondarew@users.noreply.github.com>
2023-06-25 21:27:32 +0300

    add

---
+++ README.md
@@ -0,0 +1,4 @@
+https://ibb.co/6ykRFwC
+https://ibb.co/f0bMYbT
+https://ibb.co/2j2z9CP
+https://ibb.co/v1n2cyC
--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-console.log(123)
+const contacts = require("./contacts");
+const { Command } = require("commander");
+const program = new Command();
+program
+    .option("-a, --action, <type>", "choose action")
+    .option("-i, --id, <type>", "id user")
+    .option("-n, --name, <type>", "name user")
+    .option("-e, --email, <type>", "email user")
+    .option("-p, --phone, <type>", "phone number user");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+    switch (action) {
+        case "list":
+            const allContacts = await contacts.listContacts();
+            return console.log(allContacts);
+        case "get":
+            const contactsById = await contacts.getContactById(id);
+            return console.log(contactsById);
+        case "remove":
+            const removeContact = await contacts.removeContact(id);
+            return console.log(removeContact);
+        case "add":
+            const addContact = await contacts.addContact(
+                id,
+                name,
+                email,
+                phone
+            );
+            return console.log(addContact);
+        default:
+            console.warn("Unknown action type!");
+    }
+};
+
+// invokeAction({action: 'list'});
+// invokeAction({action: 'get', id: 'qdggE76Jtbfd9eWJHrssH'});
+// invokeAction({ action: "remove", id: "1DEXoP8AuCGYc1YgoQ6hw" });
+// invokeAction({action: 'add', id: "1DEXoP8AuCGYc1YgoQ6hw", name: "Abbot Franks", email: "scelerisque@magnis.org", phone: "(186) 568-3720"  })
+invokeAction(argv);
